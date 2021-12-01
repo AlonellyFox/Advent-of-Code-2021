adb15d1aefd3adafc5866874e72227c423cf5835
AlonellyFox <67392206+AlonellyFox@users.noreply.github.com>
2021-12-01 21:32:29 +0100

    Update 1.py
    Removed unneccessary 'values' array

--- December 1#/1.py
+++ December 1#/1.py
@@ -1,13 +1,10 @@
-values = []
 inc_count = 0
 last_buffer = None
-
 
 with open("./input.txt", "r") as f:
     read = f.readline()
     while read != "":
         value = int(read)
-        values.append(value)
 
         if last_buffer is not None:
             if value > last_buffer:
